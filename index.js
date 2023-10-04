e0d1e1851ec3eb7eaa3c04f117b8f3bcb0c5d9b7
Rishikeshari7 <rishikeshari7@gmail.com>
2023-10-05 02:31:55 +0530

    Home Page

---
+++ index.js
@@ -0,0 +1,14 @@
+<script>
+var typeData = new Typed(".role", {
+  strings: [
+    "Web Developer",
+    "Competitive Programming",
+    "Leaner",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+</script>
