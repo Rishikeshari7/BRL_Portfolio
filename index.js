d8b2b9257f4aaa86d241442b4e31ace17af5e1bb
Rishikeshari7 <rishikeshari7@gmail.com>
2023-10-10 17:20:06 +0530

    added hamburger menu

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function clicked(){
-    window.open("http://127.0.0.1:3000/index.html")
-};
-// alert("hiii");
-/* <script>
-var typeData = new Typed(".role", {
-  strings: [
-    "Web Developer",
-    "Competitive Programming",
-    "Leaner",
-    "Coder",
-  ],
-  loop: true,
-  typeSpeed: 100,
-  backSpeed: 80,
-  backDelay: 1000,
-});
-</script> */
+
+// ----------------------
+$('#hamburger').click(function() {
+    $('.hamburger_menu').addClass('myClass');
+  });
+  $('.your_class').click(function() {
+    $('.hamburger_menu').addClass('yourclass');
+  });
+  
+  
+
+// Alignment issues fix
+// Add content to projects 
+// download resume 
+// make contact us page working
+// effect typewriting
+// hamburger menu not working
+
+// regex - regular expressions
